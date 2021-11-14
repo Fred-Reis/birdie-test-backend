b8703847dc67074e7ca10018f6ab1f4ff541f087
Frederico Reis <reis@devgrid.co.uk>
2021-11-14 18:25:40 -0300

    feat: swagger docs implemented

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -2,8 +2,12 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import { createConnection } from "typeorm";
+
 import { eventRouters } from "../routes/events.routes";
+
+import swaggerFile from "../../swagger.json";
 
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -11,6 +15,8 @@ createConnection();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(eventRouters);
 
